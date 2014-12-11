67ed377e39ab8be4a803c935a2295dd65152c3e3
Radivarig <reslav.hollos@gmail.com>
2014-12-11 02:00:24 +0100

    [add]panel [wip]vertex group func

--- operator_modal_timer.py
+++ operator_modal_timer.py
@@ -10,10 +10,27 @@ bl_info = {
     "category": "Object"
 }
 
+def AddVertexGroup(name, addSelected = True):
+    bpy.ops.object.vertex_group_add()
+    
+class EdgerFunc1(bpy.types.Operator):
+    """EdgerFunc1"""
+    bl_idname = "wm.edger_func1_idname"
+    bl_label = "EdgerFunc1_label"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+    
+    def execute(self, context):
+        AddVertexGroup("asd")
+        
+        return {'FINISHED'}
+    
 class Edger(bpy.types.Operator):
     """Lock vertices on edge"""
     bl_idname = "wm.edger"
     bl_label = "Edger"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
     
     _timer = None
 
@@ -22,6 +39,11 @@ class Edger(bpy.types.Operator):
             return self.cancel(context)
 
         if event.type == 'TIMER':
+            for g in context.object.vertex_groups:
+                if g.name.startswith("_edger_"):
+                    print(dir(g))
+                    #g.vertex_group_assign()
+            
             # change theme color, silly!
             color = context.user_preferences.themes[0].view_3d.space.gradients.high_gradient
             color.s = 1.0
@@ -38,18 +60,44 @@ class Edger(bpy.types.Operator):
         context.window_manager.event_timer_remove(self._timer)
         return {'CANCELLED'}
 
+#addon_keymaps = []
+#def menu_func_edger(self, context): self.layout.operator(Edger.bl_idname)
 
+class EdgerPanel(bpy.types.Panel):
+    """Edger Panel"""
+    bl_label = "Edger"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'   #TODO
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        row.label(text="Select Edge Loop:")
+        split = layout.split()
+        col = split.column(align=True)
+        col.operator(Edger.bl_idname, text="Start Edger", icon = "GROUP_VERTEX")
+        col.operator(EdgerFunc1.bl_idname, text="Unselectable", icon = "RESTRICT_SELECT_ON")
+        row = layout.row()
+        row.label(text="bla bla bla:")
+        
+#handle the keymap
+#wm = bpy.context.window_manager
+#km = wm.keyconfigs.addon.keymaps.new(name='UV Editor', space_type='EMPTY')
+#kmi = km.keymap_items.new(UvSquaresByShape.bl_idname, 'E', 'PRESS', alt=True)
+#addon_keymaps.append((km, kmi))
 
 def register():
     bpy.utils.register_class(Edger)
-
+    bpy.utils.register_class(EdgerFunc1)
+    bpy.utils.register_class(EdgerPanel)
 
 def unregister():
     bpy.utils.unregister_class(Edger)
+    bpy.utils.unregister_class(EdgerFunc1)
+    bpy.utils.unregister_class(EdgerPanel)
 
 
 if __name__ == "__main__":
     register()
 
-    # test call
-    bpy.ops.wm.modal_timer_operator()
+    # start edger
+    bpy.ops.wm.edger()
